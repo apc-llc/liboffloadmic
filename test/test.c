37c4834ab4aab69570d888ee7200d5616886c9d7
Dmitry Mikushin <dmitry@kernelgen.org>
2016-03-09 11:17:10 +0300

    Adding basic MIC device(s) availability test

---
+++ test/test.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+const char * GOMP_OFFLOAD_get_name (void);
+
+int main(int argc, char* argv[])
+{
+	const char* name = GOMP_OFFLOAD_get_name();
+	printf("GOMP backend for %s\n", name);
+	
+	int num_devices = GOMP_OFFLOAD_get_num_devices();
+	printf("%d %s device(s) available\n", num_devices, name);
+	
+	if (!num_devices) return 1;
+	
+	return 0;
+}
+
