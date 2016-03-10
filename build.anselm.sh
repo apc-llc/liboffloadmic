3679a914f535a9f39ae3107cbfb551f88df9908b
Dmitry Mikushin <dmitry@kernelgen.org>
2016-03-10 08:48:55 +0300

    Fixing target instead of host in one place

--- build.anselm.sh
+++ build.anselm.sh
@@ -13,7 +13,7 @@ make install
 cd ..
 mkdir build_host
 cd build_host
-../configure --disable-multilib --build=x86_64-pc-linux-gnu --host=x86_64-pc-linux-gnu --target=x86_64-pc-linux-gnu --enable-liboffloadmic=host CXX="g++ -I$(pwd)/../runtime -I$(pwd)/../include/coi -g -O2 -D_GNU_SOURCE -DLINUX" --prefix=$(pwd)/../../install/host --enable-offload-targets=x86_64-intelmic-linux-gnu=$(pwd)/../../install/target
+../configure --disable-multilib --build=x86_64-pc-linux-gnu --host=x86_64-pc-linux-gnu --target=x86_64-pc-linux-gnu --enable-liboffloadmic=host CXX="g++ -I$(pwd)/../runtime -I$(pwd)/../include/coi -g -O2 -D_GNU_SOURCE -DLINUX" --prefix=$(pwd)/../../install/host --enable-offload-targets=x86_64-intelmic-linux-gnu=$(pwd)/../../install/host
 make -j12
 make install
 cd ../..
