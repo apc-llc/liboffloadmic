7ce52d9a39f1af3c7fa90df4b65a7d0b90ada11d
Dmitry Mikushin <dmitry@kernelgen.org>
2016-03-17 17:28:36 +0300

    Adding openmp into saxpy kernel

--- tests/saxpy/kernel.c
+++ tests/saxpy/kernel.c
@@ -1,3 +1,6 @@
+#include <omp.h>
+#include <stdio.h>
+
 void saxpy(void* vargs)
 {
 	typedef struct
@@ -11,7 +14,16 @@ void saxpy(void* vargs)
 
 	Args* args = (Args*)vargs;
 
+	#pragma omp parallel for
 	for (int i = 0; i < args->n; i++)
 		args->y[i] += args->a * args->x[i];
+
+	#pragma omp parallel
+	{
+		#pragma omp master
+		{
+			printf("Using %d threads\n", omp_get_num_threads());
+		}
+	}
 }
 
